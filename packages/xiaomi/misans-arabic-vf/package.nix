2c03ca61059aee1e233ad14e0a43dbf1185ac847
Bingshan Chang <chang@bingshan.org>
2026-07-05 03:05:19 +0800

    misans-arabic-vf: Init at 1.002-unstable-2025-07-14
    * docs/README.md: Document MiSans Arabic VF preview.
    * packages/xiaomi/misans-arabic-vf/package.nix: Init package.
    * tools/generate-font-preview-images/package.nix: Add Arabic VF
    preview sample key.

---
+++ packages/xiaomi/misans-arabic-vf/package.nix
@@ -0,0 +1,65 @@
+{
+  fetchurl,
+  lib,
+  stdenvNoCC,
+  unzip,
+  ...
+}:
+let
+  inherit (lib)
+    licenses
+    maintainers
+    platforms
+    ;
+
+  version = "1.002-unstable-2025-07-14";
+
+  src = fetchurl {
+    hash = "sha256-8sxJOdIC1sZFxcsGEz+998qkINQyUGgvnTeSiEyLcuU=";
+    url = "https://hyperos.mi.com/font-download/MiSans_Arabic.zip";
+  };
+
+  meta = {
+    description = "MiSans Arabic VF variable font";
+    homepage = "https://hyperos.mi.com/font/zh/details/arabic";
+    license = licenses.mi-sans-font-license;
+
+    longDescription = ''
+      Provides the MiSans Arabic VF variable TrueType font from
+      Xiaomi.
+
+      The package uses the MiSans Font Intellectual Property
+      License Agreement and is treated as non-redistributable in
+      this overlay. Do not publish the built font files through a
+      binary cache.
+    '';
+
+    maintainers = with maintainers; [ brsvh ];
+    platforms = platforms.all;
+    redistributable = false;
+  };
+in
+stdenvNoCC.mkDerivation {
+  inherit
+    meta
+    src
+    version
+    ;
+
+  pname = "misans-arabic-vf";
+
+  nativeBuildInputs = [
+    unzip
+  ];
+
+  sourceRoot = "MiSans Arabic/MiSans Arabic";
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm444 -t $out/share/fonts/truetype/xiaomi \
+      */MiSansArabicVF.ttf
+
+    runHook postInstall
+  '';
+}
